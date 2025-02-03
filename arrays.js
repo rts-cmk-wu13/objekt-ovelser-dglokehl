3bd79401b612ab6a3dba3a5174840f27108bcd71
Loke <U2507983@rts-365.dk>
2025-02-03 11:17:25 +0100

    opgave 1 løst

---
+++ arrays.js
@@ -0,0 +1,17 @@
+// ----- opgave 1 -----
+let loke = {
+    firstname: "Loke",
+    lastname: "Lunde",
+    age: 23,
+    city: "Copenhagen"
+}
+console.log("Opgave 1:", loke.firstname);
+console.log("Opgave 1:", loke.lastname);
+console.log("Opgave 1:", loke["age"]);
+console.log("Opgave 1:", loke["city"]);
+
+
+// ----- opgave 2 -----
+
+
+// ----- opgave 3 -----
