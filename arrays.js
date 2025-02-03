6887bb9296421a42d78256de29441f6cb74e0307
Loke <U2507983@rts-365.dk>
2025-02-03 11:30:28 +0100

    opgave 2 løst

--- arrays.js
+++ arrays.js
@@ -16,7 +16,34 @@ opgave1()
 
 
 // ----- opgave 2 -----
+function opgave2() {
+    let loke = {
+        firstname: "Loke",
+        lastname: "Lunde",
+        age: 23,
+        city: "Copenhagen",
+        married: false,
+        hobbies: ["Musik", "Spil", "Pokemon"],
+        favoriteAlbum: {
+            artist: "Bladee",
+            album: "Red Light"
+        }
+    }
 
+    console.log("Opgave 2:", loke.firstname);
+    console.log("Opgave 2:", loke.lastname);
+    console.log("Opgave 2:", loke["age"]);
+    console.log("Opgave 2:", loke["city"]);
+    console.log("Opgave 2:", loke["married"]);
+
+    console.log("Opgave 2: favoriteAlbum:", loke.favoriteAlbum.artist);
+    console.log("Opgave 2: favoriteAlbum:", loke.favoriteAlbum.album);
+
+    loke.hobbies.forEach(hobby => {
+        console.log("Opgave 2: hobbies:", hobby);
+    });
+}
+opgave2()
 
 
 // ----- opgave 3 -----
