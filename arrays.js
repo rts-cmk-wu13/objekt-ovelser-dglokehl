d7861609b89013181dec646cfcfdaf20103d60f1
Loke <U2507983@rts-365.dk>
2025-02-03 11:20:12 +0100

    opgave 1 forbedret

--- arrays.js
+++ arrays.js
@@ -1,17 +1,22 @@
 // ----- opgave 1 -----
-let loke = {
-    firstname: "Loke",
-    lastname: "Lunde",
-    age: 23,
-    city: "Copenhagen"
+function opgave1() {
+    let loke = {
+        firstname: "Loke",
+        lastname: "Lunde",
+        age: 23,
+        city: "Copenhagen"
+    }
+
+    console.log("Opgave 1:", loke.firstname);
+    console.log("Opgave 1:", loke.lastname);
+    console.log("Opgave 1:", loke["age"]);
+    console.log("Opgave 1:", loke["city"]);
 }
-console.log("Opgave 1:", loke.firstname);
-console.log("Opgave 1:", loke.lastname);
-console.log("Opgave 1:", loke["age"]);
-console.log("Opgave 1:", loke["city"]);
+opgave1()
 
 
 // ----- opgave 2 -----
+
 
 
 // ----- opgave 3 -----
